b60fa3dfccc5da3e7cd4c7745a5200d3686e4493
Ignacio Bochatay <ignaciobochatay@gmail.com>
2022-06-18 12:11:00 -0300

    Update plugins.lua

--- lua/nvim/lua/user/plugins.lua
+++ lua/nvim/lua/user/plugins.lua
@@ -44,22 +44,22 @@ return packer.startup(function(use)
 	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-	use("numToStr/Comment.nvim")
-	use("kyazdani42/nvim-web-devicons")
-	use({ "kyazdani42/nvim-tree.lua" })
-	use("akinsho/bufferline.nvim")
-	use("moll/vim-bbye")
-	use("nvim-lualine/lualine.nvim")
-	use("akinsho/toggleterm.nvim")
-	use("ahmedkhalf/project.nvim")
-	use("lewis6991/impatient.nvim")
-	use("lukas-reineke/indent-blankline.nvim")
-	use("goolord/alpha-nvim")
+	use("numToStr/Comment.nvim") -- Smart and Powerful commenting plugin for neovim
+	use("kyazdani42/nvim-web-devicons") -- This plugin provides the same icons as well as colors for each icon
+	use({ "kyazdani42/nvim-tree.lua" }) -- A File Explorer For Neovim Written In Lua
+	use("akinsho/bufferline.nvim") -- A  buffer line for Neovim built using lua
+	use("moll/vim-bbye") -- Allows you to do delete buffers without closing your windows or messing up your layout
+	use("nvim-lualine/lualine.nvim") --A blazing fast and easy to configure Neovim statusline written in Lua
+	use("akinsho/toggleterm.nvim")  -- A neovim plugin to persist and toggle multiple terminals during an editing session
+	use("ahmedkhalf/project.nvim") -- An all in one neovim plugin written in lua that provides superior project management
+	use("lewis6991/impatient.nvim") -- Improve startup time
+	use("lukas-reineke/indent-blankline.nvim") -- Adds indentation guides to all lines (including empty lines)
+	use("goolord/alpha-nvim") -- A fast and fully customizable greeter for neovim
 	use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
 
 	-- Colorschemes
 	use("folke/tokyonight.nvim")
-  use("bluz71/vim-moonfly-colors")
+  	use("bluz71/vim-moonfly-colors")
 
 	-- cmp plugins
 	use("hrsh7th/nvim-cmp") -- The completion plugin
@@ -77,10 +77,10 @@ return packer.startup(function(use)
 	use("neovim/nvim-lspconfig") -- enable LSP
 	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
 	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
-	use("RRethy/vim-illuminate")
+	use("RRethy/vim-illuminate") -- Automatically highlighting other uses of the current word under the cursor
 
 	-- Telescope
-	use("nvim-telescope/telescope.nvim")
+	use("nvim-telescope/telescope.nvim") -- Extendable fuzzy finder over lists
 
 	-- Treesitter
 	use({
@@ -89,12 +89,12 @@ return packer.startup(function(use)
 	})
 
 	-- Git
-	use("lewis6991/gitsigns.nvim")
+	use("lewis6991/gitsigns.nvim") -- Super fast git decorations
 
 	-- DAP
-	use("mfussenegger/nvim-dap")
-	use("rcarriga/nvim-dap-ui")
-	use("ravenxrz/DAPInstall.nvim")
+	use("mfussenegger/nvim-dap") -- A Debug Adapter Protocol client implementation for Neovim
+	use("rcarriga/nvim-dap-ui") -- UI for dap
+	use("ravenxrz/DAPInstall.nvim") -- A NeoVim plugin for managing several debuggers for Nvim-dap
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
