91aa7a470ebef15666b825fdf3fcc59da10cf6ad
bocha13 <ignaciobochatay@gmail.com>
2022-11-19 10:54:15 -0300

    updated tmux config and plugins, added vim-tmux-navigator for both nvim and tmux

--- lua/nvim/lua/plugins/init.lua
+++ lua/nvim/lua/plugins/init.lua
@@ -198,6 +198,9 @@ return packer.startup(function(use)
 	-- VIM BE GOOD :D
 	use("ThePrimeagen/vim-be-good")
 
+  -- tmux navigation
+  use("christoomey/vim-tmux-navigator")
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
---
+++ tmux/README.md
@@ -0,0 +1,11 @@
+## TMUX config
+
+### Tmux package manager
+first of all make sure that tpm is up to date:
+```shell
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+```
+
+### Install plugins
+inside a tmux session use the following command by default prefix is C-b
+<prefix> I
