2804a8f8d3c29670c13e61d96546c9553a737766
bocha13 <ignaciobochatay@gmail.com>
2023-02-10 18:03:46 -0300

    replaced lsp-zero with a proper config

--- nvim/lua/bocha13/plugins/lsp.lua
+++ nvim/lua/bocha13/plugins/lsp.lua
@@ -1,98 +1,218 @@
 return {
-    -- lspconfig
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v1.x",
-        dependencies = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" }, -- Required
-            { "williamboman/mason.nvim" }, -- Optional
-            { "williamboman/mason-lspconfig.nvim" }, -- Optional
+	-- LSP support
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" }, -- Required
-            { "hrsh7th/cmp-nvim-lsp" }, -- Required
-            { "hrsh7th/cmp-buffer" }, -- Optional
-            { "hrsh7th/cmp-path" }, -- Optional
-            { "saadparwaiz1/cmp_luasnip" }, -- Optional
-            { "hrsh7th/cmp-nvim-lua" }, -- Optional
+			local capabilities = cmp_nvim_lsp.default_capabilities()
 
-            -- Snippets
-            { "L3MON4D3/LuaSnip" }, -- Required
-            { "rafamadriz/friendly-snippets" }, -- Optional
-        },
-        config = function()
-          local lsp = require("lsp-zero")
-          local cmp = require("cmp")
-          local luasnip = require("luasnip")
+			-- Change the Diagnostic symbols in the sign column (gutter)
+			-- (not in youtube nvim video)
+			local signs = { Error = "E", Warn = "W", Hint = "H", Info = "E" }
+			for type, icon in pairs(signs) do
+				local hl = "DiagnosticSign" .. type
+				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+			end
 
-          cmp.setup {
-              snippet = {
-                  expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                  end
-              },
-              sources = {
-                  { name = "copilot",  group_index = 2 },
-                  { name = "luasnip",  group_index = 3 },
-                  { name = "buffer",   group_index = 2 },
-                  { name = "nvim_lsp", group_index = 2 },
-                  { name = "path",     group_index = 2 },
-              }
-          }
-          vim.diagnostic.config({
-              underline = true,
-              update_in_insert = true,
-              virtual_text = { spacing = 4, prefix = "●" },
-              severity_sort = true
-          })
+			-- configure tsserver
+			lspconfig["tsserver"].setup({
+				capabilities = capabilities,
+			})
 
-          local cmp_select   = { behavior = cmp.SelectBehavior.Select }
-          local cmp_mappings = lsp.defaults.cmp_mappings({
-                  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                  ['<C-Space>'] = cmp.mapping.complete(),
-                  ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                  ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                      cmp.select_next_item()
-                    else
-                      fallback()
-                    end
-                  end, {
-                      "i",
-                      "s",
-                  }),
-              })
+			-- configure rust_analyzer server
+			lspconfig["rust_analyzer"].setup({
+				capabilities = capabilities,
+				cmd = {
+					"rustup",
+					"run",
+					"stable",
+					"rust-analyzer",
+				},
+			})
 
-          lsp.preset("recommended")
-          lsp.ensure_installed({
-              "tsserver",
-              "eslint",
-              "sumneko_lua",
-              "rust_analyzer"
-          })
-          lsp.set_preferences({
-              sign_icons = {
-                  error = "E",
-                  warn = "W",
-                  hint = "H",
-                  info = "I"
-              }
-          })
-          lsp.setup_nvim_cmp({
-              completion = {
-                  completeopt = "menu,menuone,noinsert,noselect"
-              },
-              mapping = cmp_mappings,
-              confirm_opts = {
-                  behavior = cmp.ConfirmBehavior.Replace,
-                  select = false,
-              },
-          })
+			-- configure gopls
+			lspconfig["gopls"].setup({
+				capabilities = capabilities,
+			})
 
-          lsp.setup()
-        end
-    },
+			-- configure lua server (with special settings)
+			lspconfig["sumneko_lua"].setup({
+				capabilities = capabilities,
+				settings = { -- custom settings for lua
+					Lua = {
+						-- make the language server recognize "vim" global
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							-- make language server aware of runtime files
+							library = {
+								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+								[vim.fn.stdpath("config") .. "/lua"] = true,
+							},
+						},
+					},
+				},
+			})
+
+			-- configure rust_analyzer server
+			local rt_status_ok, rt = pcall(require, "rust-tools")
+			if not rt_status_ok then
+				print("no rust-tools")
+				return
+			end
+
+			local rust_opts = {
+				tools = {
+					autoSetHints = false,
+					hover_actions = { border = false },
+					cache = true,
+				},
+				server = {
+					capabilities = capabilities,
+					cmd = {
+						"rustup",
+						"run",
+						"stable",
+						"rust-analyzer",
+					},
+					settings = {
+						["rust-analyzer"] = {
+							diagnostics = {
+								experimental = true,
+							},
+						},
+					},
+				},
+			}
+			rt.setup(rust_opts)
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			local mason = require("mason")
+			local mason_lsp = require("mason-lspconfig")
+
+			local config = {
+				virtual_text = true,
+				update_in_insert = true,
+				underline = true,
+				severity_sort = true,
+				float = {
+					focusable = true,
+					style = "minimal",
+				},
+			}
+
+			vim.diagnostic.config(config)
+			mason.setup()
+
+			mason_lsp.setup({
+				ensure_installed = {
+					"sumneko_lua",
+					"rust_analyzer",
+					"tsserver",
+					"eslint",
+				},
+				automatic_installation = true,
+			})
+		end,
+	},
+	{ "williamboman/mason-lspconfig.nvim" },
+	{ "simrat39/rust-tools.nvim" },
+
+	-- Autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				sources = {
+					{ name = "copilot", group_index = 2 },
+					{ name = "luasnip", group_index = 3 },
+					{ name = "buffer", group_index = 2 },
+					{ name = "nvim_lsp", group_index = 2 },
+					{ name = "path", group_index = 2 },
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+				}),
+			})
+		end,
+	},
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "saadparwaiz1/cmp_luasnip" },
+	{ "hrsh7th/cmp-nvim-lua" },
+
+	-- Snippets
+	{ "L3MON4D3/LuaSnip" }, -- Required
+	{ "rafamadriz/friendly-snippets" }, -- Optional
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			local formatting = null_ls.builtins.formatting
+			local diagnostics = null_ls.builtins.diagnostics
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+			null_ls.setup({
+				sources = {
+					formatting.prettier,
+					formatting.stylua,
+					diagnostics.eslint_d.with({
+						-- only enable islint if root has .eslintrc.js
+						confition = function(utils)
+							return utils.root_has_file(".eslintrc.js")
+						end,
+					}),
+				},
+				-- configure format on save
+				on_attach = function(current_client, bufnr)
+					if current_client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({
+									filter = function(client)
+										--  only use null-ls for formatting instead of lsp server
+										return client.name == "null-ls"
+									end,
+									bufnr = bufnr,
+								})
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
 }
--- nvim/lua/bocha13/plugins/ui.lua
+++ nvim/lua/bocha13/plugins/ui.lua
@@ -27,13 +27,13 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "VeryLazy",
-		opts = function(plugin)
+		opts = function()
 			local icons = {
 				diagnostics = {
-					Error = " ",
-					Warn = " ",
-					Hint = " ",
-					Info = " ",
+					Error = "E ",
+					Warn = "W ",
+					Hint = "H ",
+					Info = "I ",
 				},
 				git = {
 					added = " ",
@@ -137,7 +137,7 @@ return {
 		config = function()
 			require("neo-tree").setup({
 				close_if_last_window = true,
-				enable_diagnostics = false,
+				enable_diagnostics = true,
 				source_selector = {
 					winbar = true,
 					content_layout = "center",
@@ -159,9 +159,9 @@ return {
 					},
 					git_status = {
 						symbols = {
-							added = "",
-							deleted = "",
-							modified = "",
+							added = "",
+							deleted = "",
+							modified = "",
 							renamed = "",
 							untracked = "",
 							ignored = "",
@@ -242,21 +242,16 @@ return {
 			require("bufferline").setup({
 				-- Enable/disable animations
 				animation = false,
-
 				-- Enable/disable auto-hiding the tab bar when there is a single buffer
 				auto_hide = false,
-
 				-- Enable/disable current/total tabpages indicator (top right corner)
 				tabpages = true,
-
 				-- Enable/disable close button
 				closable = true,
-
 				-- Enables/disable clickable tabs
 				--  - left-click: go to buffer
 				--  - middle-click: delete buffer
 				clickable = true,
-
 				-- Enables / disables diagnostic symbols
 				diagnostics = {
 					-- you can use a list
@@ -271,7 +266,6 @@ return {
 					[vim.diagnostic.severity.INFO] = { enabled = false },
 					[vim.diagnostic.severity.HINT] = { enabled = true },
 				},
-
 				-- Excludes buffers from the tabline
 				-- exclude_ft = { "javascript" },
 				-- exclude_name = { "package.json" },
@@ -281,60 +275,62 @@ return {
 
 				-- Disable highlighting alternate buffers
 				highlight_alternate = false,
-
 				-- Disable highlighting file icons in inactive buffers
 				highlight_inactive_file_icons = false,
-
 				-- Enable highlighting visible buffers
 				highlight_visible = true,
-
 				-- Enable/disable icons
 				-- if set to 'numbers', will show buffer index in the tabline
 				-- if set to 'both', will show buffer index and icons in the tabline
 				icons = true,
-
 				-- If set, the icon color will follow its corresponding buffer
 				-- highlight group. By default, the Buffer*Icon group is linked to the
 				-- Buffer* group (see Highlighting below). Otherwise, it will take its
 				-- default value as defined by devicons.
 				icon_custom_colors = false,
-
 				-- Configure icons on the bufferline.
 				icon_separator_active = "▎",
 				icon_separator_inactive = "▎",
 				icon_close_tab = "",
 				icon_close_tab_modified = "●",
 				icon_pinned = "車",
-
 				-- If true, new buffers will be inserted at the start/end of the list.
 				-- Default is to insert after current buffer.
 				insert_at_end = false,
 				insert_at_start = false,
-
 				-- Sets the maximum padding width with which to surround each tab
 				maximum_padding = 1,
-
 				-- Sets the minimum padding width with which to surround each tab
 				minimum_padding = 1,
-
 				-- Sets the maximum buffer name length.
 				maximum_length = 30,
-
 				-- If set, the letters for each buffer in buffer-pick mode will be
 				-- assigned based on their name. Otherwise or in case all letters are
 				-- already assigned, the behavior is to assign letters in order of
 				-- usability (see order below)
 				semantic_letters = true,
-
 				-- New buffer letters are assigned in this order. This order is
 				-- optimal for the qwerty keyboard layout but might need adjustement
 				-- for other layouts.
 				letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
-
 				-- Sets the name of unnamed buffers. By default format is "[Buffer X]"
 				-- where X is the buffer number. But only a static string is accepted here.
 				no_name_title = nil,
 			})
+		end,
+	},
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+
+	-- ALPHA -- home page -- greeter
+	{
+		"goolord/alpha-nvim",
+		config = function()
+			require("alpha").setup(require("alpha.themes.dashboard").config)
 		end,
 	},
 }
