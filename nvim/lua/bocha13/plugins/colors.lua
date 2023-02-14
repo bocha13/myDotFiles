72480b8b9d0b9e72fe0149900ebb4661d3121a6f
bocha13 <ignaciobochatay@gmail.com>
2023-02-14 20:00:52 -0300

    update colorscheme

--- nvim/lua/bocha13/plugins/colors.lua
+++ nvim/lua/bocha13/plugins/colors.lua
@@ -2,38 +2,45 @@ return {
 	"folke/tokyonight.nvim",
 	config = function()
 		require("tokyonight").setup({
-			style = "moon",
+			style = "night",
 			on_colors = function(colors)
-				colors.bg = "#1e2030"
+				-- colors.bg = "#1e2030"
 				-- colors.bg_visual = "#292e42"
 				-- colors.bg_dark = "#1c1c1c"
 				-- colors.bg_float = "#1c1c1c"
 				-- colors.bg_popup = "#1c1c1c"
 				-- colors.bg_sidebar = "#1c1c1c"
-				colors.bg_statusline = "#161824"
+				-- colors.bg_statusline = "#161824"
 				colors.border = "#65bcff"
 			end,
 			on_highlights = function(highlight, c)
-				highlight.BufferTabpageFill = {
-					bg = c.bg_statusline,
-					fg = "#3b4261",
-				}
-				highlight.BufferOffset = {
-					bg = c.bg_statusline,
-					fg = "#65bcff",
-				}
-				highlight.BufferInactiveSign = {
-					bg = "#161824",
-					fg = "#65bcff",
-				}
-				highlight.BufferCurrentSign = {
-					bg = "#3b4261",
-					fg = "#65bcff",
-				}
-				highlight.MsgArea = {
+				-- mini.indent line color
+				highlight.MiniIndentscopeSymbol = {
 					bg = c.bg,
-					fg = "#65bcff",
+					fg = c.border,
 				}
+				-- Bufferline colors
+				-- 	highlight.BufferTabpageFill = {
+				-- 		bg = c.bg_statusline,
+				-- 		fg = "#3b4261",
+				-- 	}
+				-- 	highlight.BufferOffset = {
+				-- 		bg = c.bg_statusline,
+				-- 		fg = "#65bcff",
+				-- 	}
+				-- 	highlight.BufferInactiveSign = {
+				-- 		bg = "#161824",
+				-- 		fg = "#65bcff",
+				-- 	}
+				-- 	highlight.BufferCurrentSign = {
+				-- 		bg = "#3b4261",
+				-- 		fg = "#65bcff",
+				-- 	}
+				-- 	Message area colors
+				-- 	highlight.MsgArea = {
+				-- 		bg = c.bg,
+				-- 		fg = "#65bcff",
+				-- 	}
 			end,
 		})
 		vim.cmd.colorscheme("tokyonight")
