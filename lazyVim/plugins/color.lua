18fd2d5b38c5988a6a90a533c1f9fe2c9dbc8af4
bocha13 <ignaciobochatay@gmail.com>
2023-06-14 11:29:52 -0300

    updated lualine and removed unused configs

--- lazyVim/plugins/color.lua
+++ lazyVim/plugins/color.lua
@@ -1,36 +1,24 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "night",
-      on_colors = function(c)
-        c.gitSigns = {
-          add = "#8fc28c",
-          change = "#7495d1",
-          delete = "#f38ba8",
-        }
-      end,
-    },
-    -- config = function()
-    --   require("tokyonight").setup({
-    --     style = "night",
-    --     on_colors = function(c)
-    --       c.gitSigns = {
-    --         add = "#8fc28c",
-    --         change = "#7495d1",
-    --         delete = "#f38ba8",
-    --       }
-    --     end,
-    --   })
-    -- end,
-  },
-  -- Configure LazyVim to load the colorscheme
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = "night",
+			on_colors = function(c)
+				c.gitSigns = {
+					add = "#8fc28c",
+					change = "#7495d1",
+					delete = "#f38ba8",
+				}
+			end,
+		},
+	},
+	-- Configure LazyVim to load the colorscheme
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "tokyonight",
+		},
+	},
 }
