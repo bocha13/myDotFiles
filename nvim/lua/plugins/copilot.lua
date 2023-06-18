16f72c396c44a932542e1cb3618bc8da14919a5a
bocha13 <ignaciobochatay@gmail.com>
2023-06-18 00:11:34 -0300

    added chatGPT plugin

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -14,4 +14,16 @@ return {
     "zbirenbaum/copilot-cmp",
     opts = {},
   },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
 }
