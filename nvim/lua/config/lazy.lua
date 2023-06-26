e6a7ba3eb8b4d85f7285cad45826bceeae1dc171
bocha13 <ignaciobochatay@gmail.com>
2023-06-26 20:22:40 -0300

    add tailwindcss config, removed unnecessary copilot file

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.dap.nlua" },
     { import = "lazyvim.plugins.extras.dap.core" },
     -- import/override with your plugins
