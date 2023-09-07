92a5d4496e0c6249f552d7a471b296afc778934d
bocha13 <ignaciobochatay@gmail.com>
2023-09-07 19:03:13 -0300

    updated lazy, lsp and mason configs

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -71,6 +71,11 @@ return {
       on_attach = on_attach,
     })
 
+    lspconfig["gopls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
+
     lspconfig["tailwindcss"].setup({
       capabilities = capabilities,
       on_attach = on_attach,
--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -20,7 +20,16 @@ return {
       end,
     },
     init = function()
-      vim.notify = require("notify")
+      -- this hack is to avoid showing the "No information available" message
+      local banned_messages = { "No information available" }
+      vim.notify = function(msg, ...)
+        for _, banned in ipairs(banned_messages) do
+          if msg == banned then
+            return
+          end
+        end
+        return require("notify")(msg, ...)
+      end
     end,
   }
 }
