ad72d26e82a3abb54cc4d43b7987e8aa7c08c141
bocha13 <ignaciobochatay@gmail.com>
2023-10-31 17:44:50 -0300

    updated colorscheme for nvim-lazy

--- nvim-lazy/lua/plugins/color.lua
+++ nvim-lazy/lua/plugins/color.lua
@@ -1,23 +1,34 @@
 return {
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "tokyonight",
+  --   },
+  -- },
+  --
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {
+  --     style = "night",
+  --     on_colors = function(c)
+  --       c.gitSigns = {
+  --         add = "#8fc28c",
+  --         change = "#7495d1",
+  --         delete = "#f38ba8",
+  --       }
+  --     end,
+  --   },
+  -- },
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
+    "sainnhe/gruvbox-material",
     lazy = false,
     priority = 1000,
-    opts = {
-      style = "night",
-      on_colors = function(c)
-        c.gitSigns = {
-          add = "#8fc28c",
-          change = "#7495d1",
-          delete = "#f38ba8",
-        }
-      end,
-    },
+    config = function()
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
   },
 }
