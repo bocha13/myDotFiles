ab20a0543e9e2188ac7240e3fd930010f0effb51
bocha13 <ignaciobochatay@gmail.com>
2024-04-14 15:18:49 -0300

    add back neo-tree, update ui to neo-tree bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,45 +1,47 @@
 return {
-	{
-		"akinsho/bufferline.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		dependencies = "nvim-tree/nvim-web-devicons",
-		keys = {
-			{ "<S-h>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Previous Buffer" },
-			{ "<S-l>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next Buffer" },
-			{ "<A-p>", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-			{ "<A-C>", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-		},
-		opts = {
-			options = {
-				-- mode = "buffers",
-				close_command = function(n)
-					require("mini.bufremove").delete(n, false)
-				end,
-				middle_mouse_command = function(n)
-					require("mini.bufremove").delete(n, false)
-				end,
-				diagnostics = "nvim_lsp",
-				always_show_bufferline = false,
-				diagnostics_indicator = function(_, _, diag)
-					local icons = {
-						Error = "E",
-						Warn = "W",
-						Hint = "H",
-						Info = "I",
-					}
-					local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-						.. (diag.warning and icons.Warn .. diag.warning or "")
-					return vim.trim(ret)
-				end,
-				offsets = {
-					{
-						filetype = "neo-tree",
-						text = "Explorer",
-						highlight = "Directory",
-						text_align = "center",
-					},
-				},
-			},
-		},
-	},
+  {
+    "akinsho/bufferline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    keys = {
+      { "<S-h>", "<Cmd>BufferLineCyclePrev<CR>",            desc = "Previous Buffer" },
+      { "<S-l>", "<Cmd>BufferLineCycleNext<CR>",            desc = "Next Buffer" },
+      { "<A-p>", "<Cmd>BufferLineTogglePin<CR>",            desc = "Toggle pin" },
+      { "<A-C>", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+    },
+    opts = {
+      options = {
+        -- mode = "buffers",
+        indicator_icon = " ",
+        show_close_icon = false,
+        show_buffer_icons = false,
+        close_command = function(n)
+          require("mini.bufremove").delete(n, false)
+        end,
+        middle_mouse_command = function(n)
+          require("mini.bufremove").delete(n, false)
+        end,
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = {
+            Error = "E",
+            Warn = "W",
+            Hint = "H",
+            Info = "I",
+          }
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+              .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Explorer",
+            highlight = "Directory",
+            text_align = "center",
+          },
+        },
+      },
+    },
+  },
 }
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,33 +1,33 @@
 return {
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("tokyonight").setup({
-	-- 			style = "night",
-	-- 			on_colors = function(c)
-	-- 				c.gitSigns = {
-	-- 					add = "#8fc28c",
-	-- 					change = "#7495d1",
-	-- 					delete = "#f38ba8",
-	-- 				}
-	-- 			end,
-	-- 		})
-	-- 		vim.cmd.colorscheme("tokyonight")
-	-- 	end,
-	-- },
-	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.gruvbox_material_better_performance = 1
-			vim.g.gruvbox_material_background = "hard"
-			vim.g.gruvbox_material_foreground = "mix"
-			vim.g.gruvbox_material_diagnostic_line_highlight = 1
-			vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
-			vim.cmd.colorscheme("gruvbox-material")
-		end,
-	},
+  -- {
+  -- 	"folke/tokyonight.nvim",
+  -- 	lazy = false,
+  -- 	priority = 1000,
+  -- 	config = function()
+  -- 		require("tokyonight").setup({
+  -- 			style = "night",
+  -- 			on_colors = function(c)
+  -- 				c.gitSigns = {
+  -- 					add = "#8fc28c",
+  -- 					change = "#7495d1",
+  -- 					delete = "#f38ba8",
+  -- 				}
+  -- 			end,
+  -- 		})
+  -- 		vim.cmd.colorscheme("tokyonight")
+  -- 	end,
+  -- },
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_foreground = "mix"
+      vim.g.gruvbox_material_diagnostic_line_highlight = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
 }
