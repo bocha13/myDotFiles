1c90b48af2826425f96e2fce5e0528d5bbb6d4cc
bocha13 <ignaciobochatay@gmail.com>
2024-12-09 19:48:19 -0300

    add autocmd to highlight text on yank, comment out some conform configs

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -29,6 +29,15 @@ vim.api.nvim_create_autocmd("BufEnter", {
   desc = "Set Jenkinsfile filetype",
 })
 
+-- Highlight when yanking text
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end
+})
+
 vim.opt.showmode = false
 vim.opt.nu = true
 vim.opt.relativenumber = true
--- nvim/lua/plugins/lsp/formatting.lua
+++ nvim/lua/plugins/lsp/formatting.lua
@@ -2,19 +2,19 @@ return {
   {
     "stevearc/conform.nvim",
     dependencies = { "mason.nvim" },
-    -- lazy = true,
-    -- event = { "BufReadPre", "BufNewFile" },
+    lazy = true,
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
       local conform = require("conform")
 
       conform.setup({
         formatters_by_ft = {
           -- javascript = { "prettier" },
-          typescript = { "prettier" },
+          -- typescript = { "prettier" },
           javascriptreact = { "prettier" },
           typescriptreact = { "prettier" },
           css = { "prettier" },
-          html = { "prettier" },
+          -- html = { "prettier" },
           json = { "prettier" },
           yaml = { "prettier" },
           -- markdown = { "prettier" },
