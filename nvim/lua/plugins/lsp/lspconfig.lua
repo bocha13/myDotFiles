a5ea293d2f73cdab79b41d127caa1b7ad20ed6b2
bocha13 <ignaciobochatay@gmail.com>
2025-02-03 22:44:51 -0300

    removed mason file, mason config moved to lspconfig to have setupe_handlers do all the work instead of maintaining a serverList object

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -5,10 +5,14 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
   },
   config = function()
     local lspconfig = require("lspconfig")
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
 
     -- set keymaps
     local keymap = vim.keymap
@@ -17,8 +21,7 @@ return {
 
       vim.lsp.inlay_hint.enable(true)
 
-      keymap.set('n', '<leader>q', diagnostics_list,
-        { noremap = true, silent = true })
+      keymap.set('n', '<leader>q', diagnostics_list, { noremap = true, silent = true })
       keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
       keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
       keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
@@ -37,7 +40,6 @@ return {
       end
     end
 
-
     -- used to enable autocompletion
     local capabilities = cmp_nvim_lsp.default_capabilities()
     local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
@@ -52,44 +54,45 @@ return {
       on_attach = on_attach,
     }
 
-    -- add servers to this list and they will be automatically setup
-    local serverList = {
-      ["astro"] = defaultOpts,
-      ["html"] = defaultOpts,
-      ["cssls"] = defaultOpts,
-      ["graphql"] = defaultOpts,
-      ["ts_ls"] = defaultOpts,
-      ["tailwindcss"] = defaultOpts,
-      ["gopls"] = defaultOpts,
-      ["clangd"] = defaultOpts,
-      -- ["clangd"] = {
-      --   on_attach = on_attach,
-      --   capabilities = capabilities,
-      --   cmd = { "clangd", "--query-driver=/usr/bin/arm-none-eabi-*", "--compile-commands-dir=build" }, -- Adjust flags as needed
-      -- },
-      ["eslint"] = defaultOpts,
-      ["jsonls"] = defaultOpts,
-      ["lua_ls"] = {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim", "general" },
-            },
-            workspace = {
-              library = {
-                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                [vim.fn.stdpath("config") .. "/lua"] = true,
+    mason.setup()
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "astro",
+        "html",
+        "cssls",
+        "graphql",
+        "ts_ls",
+        "tailwindcss",
+        "gopls",
+        "clangd",
+        "eslint",
+        "jsonls",
+        "lua_ls"
+      },
+    })
+
+    -- Automatically setup lsp in mason config
+    mason_lspconfig.setup_handlers({
+      function(server_name)
+        lspconfig[server_name].setup(defaultOpts)
+      end,
+      ["lua_ls"] = function()
+        lspconfig.lua_ls.setup(vim.tbl_deep_extend("force", defaultOpts, {
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim", "general" },
+              },
+              workspace = {
+                library = {
+                  [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                  [vim.fn.stdpath("config") .. "/lua"] = true,
+                },
               },
             },
           },
-        },
-      },
-    }
-
-    for server, config in pairs(serverList) do
-      lspconfig[server].setup(config)
-    end
+        }))
+      end,
+    })
   end,
 }
