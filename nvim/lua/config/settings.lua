1fe06f2fb274444fda4761a7ad098b2045f02e09
bocha13 <ignaciobochatay@gmail.com>
2025-04-23 15:56:51 +0000

    moved win32yank to settings file

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -38,6 +38,29 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end
 })
 
+-- this is to have yank in wsl, requires the following dependencies
+-- install in WSL:
+-- curl -sLo /tmp/win32yank.zip https://github.com/equalsraf/win32yank/releases/latest/download/win32yank-x64.zip
+-- unzip -p /tmp/win32yank.zip win32yank.exe > ~/.local/bin/win32yank.exe
+-- chmod +x ~/.local/bin/win32yank.exe
+
+--then add this to the path:
+-- export PATH="$HOME/.local/bin:$PATH"
+if vim.fn.executable('win32yank.exe') == 1 then
+  vim.g.clipboard = {
+    name = 'win32yank-wsl',
+    copy = {
+      ['+'] = 'win32yank.exe -i --crlf',
+      ['*'] = 'win32yank.exe -i --crlf',
+    },
+    paste = {
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = false,
+  }
+end
+
 vim.opt.showmode = false
 vim.opt.nu = true
 vim.opt.relativenumber = true
